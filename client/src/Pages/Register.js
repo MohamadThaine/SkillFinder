7cca3e343a1f8f7288aa4a950f99117f185c93b0
Mohamad Tahaina <hamwd21@gmail.com>
2023-04-20 02:08:58 +0300

    start working in register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import '../Assets/Styles/Register.css'
+
 function Register(){
+    const [isOwner, setIsOwner] = useState('');
+    return(
+        <div className="container text-center mt-5">
+            <h3>Would you like to Register as?</h3>
+            <div className="row mt-3 d-flex justify-content-center">
+                <button className="col-md-5 register-type-btn" onClick={() => setIsOwner('true')}>
+                    Owner
+                </button>
+                <button className="col-md-5 register-type-btn" onClick={() => setIsOwner('false')}>
+                    Apprentice
+                </button>
+            </div>
+            <div className="row mt-5">
+                {isOwner == 'true' && <RegisterForOwner />}
+                {isOwner == 'false' && <RegisterForApprentice />}
+            </div>
+        </div>
+    )
+}
+
+const RegisterForOwner = ({inputs}) => {
     return(
         <>
-         this is register!
+         Register For Owner 
+        </>
+    )
+}
+
+const RegisterForApprentice = ({inputs}) => {
+    return(
+        <>
+         Register For Apprentice
         </>
     )
 }
