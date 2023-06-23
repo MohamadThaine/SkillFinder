87f5ca8672069c3d80d0bdd368f9c05d1de6307d
Mohamad tahaina <hamwd21@gmail.com>
2023-06-24 02:32:21 +0300

    Added contact to student table

--- client/src/Component/StudentTable.js
+++ client/src/Component/StudentTable.js
@@ -1,9 +1,11 @@
-import { Box, Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow  } from "@mui/material"
+import { Box, Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import { useEffect, useState } from "react";
+import ContactApprentice from "./ContactApprentice";
 
 const StudentTable = ({ open, handleClose, appID }) => {
     const [students, setStudents] = useState([]);
-
+    const [choosedStudent, setChoosedStudent] = useState(null);
+    const [openSendMessage, setOpenSendMessage] = useState(false);
     useEffect(() => {
         fetch(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/getAppStudents/${appID}`, {
             method: 'GET',
@@ -31,6 +33,7 @@ const StudentTable = ({ open, handleClose, appID }) => {
                             <TableRow>
                                 <TableCell align="center">Name</TableCell>
                                 <TableCell align="center">Enrolled</TableCell>
+                                <TableCell align="center">Contact</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -41,10 +44,19 @@ const StudentTable = ({ open, handleClose, appID }) => {
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     <TableCell key={row.ID} align="center">
-                                    {row.apprentice.User.Name}
+                                        {row.apprentice.User.Name}
                                     </TableCell>
                                     <TableCell key={row.ID} align="center">
                                         {row.Date}
+                                    </TableCell>
+                                    <TableCell key={row.ID} align="center">
+                                        <Button variant="contained" onClick={() => {
+                                            setChoosedStudent({
+                                                ID: row.apprentice.User.ID,
+                                                Name: row.apprentice.User.Name
+                                            });
+                                            setOpenSendMessage(true);
+                                        }}>Contact</Button>
                                     </TableCell>
                                 </TableRow>
                             ))}
@@ -52,10 +64,12 @@ const StudentTable = ({ open, handleClose, appID }) => {
                     </Table>
                 </TableContainer>
                 <Button variant="contained" className="mt-3" onClick={handleClose}>Close</Button>
+                {choosedStudent && <ContactApprentice open={openSendMessage} handleClose={() => setOpenSendMessage(false)} user={choosedStudent} />}
             </Box>
         </Modal>
     )
 }
+
 
 export default StudentTable
 
